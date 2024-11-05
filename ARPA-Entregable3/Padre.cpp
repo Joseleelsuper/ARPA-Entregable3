6ad62b504fd9c1dc565b46bfebbb9286724a9397
Joseleelsuper <tory002254@gmail.com>
2024-11-05 16:03:23 +0100

    a

--- ARPA-Entregable3/Padre.cpp
+++ ARPA-Entregable3/Padre.cpp
@@ -32,12 +32,34 @@ int main(int argc, char* argv[]) {
 
        // Enviar mensaje de saludo a los hijos  
        char mensaje[] = "<<PADRE>> Hola desde el padre";  
-       for (int i = 0; i < NUM_HIJOS; i++) {  
-           MPI_Send(mensaje, sizeof(mensaje), MPI_CHAR, i, TAG, intercom);
-       }
-	   MPI_Send(&start_time, 1, MPI_DOUBLE, 0, TAG+1, intercom);
-   }
+	   MPI_Bcast(mensaje, sizeof(mensaje), MPI_CHAR, MPI_ROOT, intercom);
+	   MPI_Send(&start_time, 1, MPI_DOUBLE, 0, TAG, intercom);
+   }  
  
    MPI_Finalize();  
    return 0;  
 }
+
+/*
+ #include <mpi.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+   MPI_Init(&argc, &argv);
+
+   int numHijos = 4; // Número de procesos hijo a lanzar
+   MPI_Comm intercom;
+
+   // Convertir el nombre del programa hijo a char*
+   char hijo[] = "ARPA-Entregable3-Hijos.exe";
+   MPI_Comm_spawn(hijo, MPI_ARGV_NULL, numHijos, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &intercom, MPI_ERRCODES_IGNORE);
+
+   printf("\n<<PADRE>> Soy el Padre\n");
+   // Enviar mensaje de saludo a los hijos
+   const char* mensajePadre = "<<PADRE>> Saludo a los hijos";
+   MPI_Bcast((void*)mensajePadre, 100, MPI_CHAR, MPI_ROOT, intercom);
+
+   MPI_Finalize();
+   return 0;
+}
+*/
